b40cc66af8fb8def6b1e96320dc6b017711af71b
Sanket Kedare <sanketkedare200@gmail.com>
2025-03-04 12:14:04 +0530

    Under Mantenance

--- src/components/SignIn-Pop-Up/utils.js
+++
@@ -1,6 +0,0 @@
-export const benefits = [
-  "Receive free career counseling from experts",
-  "Receive customized recommendations",
-  "Upload documents for verification from experts",
-  "Monitor application progress",
-];
